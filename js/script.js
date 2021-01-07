0c7940e4ca1146d9eb77a90df778a7ba9796385a
Vlad Meleshko <vladik4.98@mail.ru>
2021-01-07 21:23:30 +0300

    feat: add babel and polyfills

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
@@ -5,7 +8,7 @@ import cards from './modules/cards';
 import calc from './modules/calc';
 import forms from './modules/forms';
 import slider from './modules/slider';
-import {openModal} from './modules/modal'
+import {openModal} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
 
